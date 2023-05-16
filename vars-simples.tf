06be225022034b407483a4192d13b36dfc4a2761
lsoguma <124442926+lsoguma@users.noreply.github.com>
2023-05-15 21:32:29 -0300

    Update vars-simples.tf

--- vars-simples.tf
+++ vars-simples.tf
@@ -1,8 +1,21 @@
 variable "pais" {
-  type = string
-  description ="Informe o seu pais"
+        type = string
+        description = "Informe o seu pais"
 }
 
+variable "idade" {
+        type = number
+        description = "Informe sua idade"
+}
+
+variable "check" {
+        type = bool
+        default = true
+        description = "Informe necessidade de checagem"
+}
+
+################## OUTPUT SESSION ##################
+
 output "pais" {
-  value =var.pais
+        value = var.pais
 }
